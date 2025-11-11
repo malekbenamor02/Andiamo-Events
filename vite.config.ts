0aeec323efbaa8bbf4886fd82e2f6483e2c59ad2
malek ben amor <fmalekbenamorf@gmail.com>
2025-11-12 00:14:42 +0100

    Simplify vercel.json and ensure Vite base path is correct

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/',
   server: {
     host: "::",
     port: 3000,
@@ -28,5 +28,6 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    emptyOutDir: true,
   },
 }));
